d13d39c40be8ce692fadd92714431ab299a68928
Jansen Junior <jsd.alcantara@gmail.com>
2022-03-21 22:49:38 -0400

    feat: basic source, manga, chapter types

--- ebi_sources/lib/lib.rs
+++ ebi_sources/lib/lib.rs
@@ -1,3 +1,26 @@
 pub mod client;
-
 pub mod opex;
+
+pub struct Source {
+    name: String,
+    title: String,
+    description: String,
+    base_url: String,
+}
+
+pub struct Manga {
+    id: usize,
+    name: String,
+    title: String,
+    thumbnail: String,
+    url: String,
+    source_name: String,
+}
+
+pub struct Chapter {
+    id: usize,
+    mangaId: usize,
+    title: String,
+    url: String,
+    source_name: String,
+}
