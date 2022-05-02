dc112e1dcda1f64e8ab6f0d2765bba9d58bae410
hadara <vlopes45@gmail.com>
2022-05-02 00:08:36 -0400

    :zap: MADE IT ASYNC!

--- ebi_archive/lib/downloader.rs
+++ ebi_archive/lib/downloader.rs
@@ -1,12 +1,13 @@
 use crate::errors::download::DownloadError;
 use crate::Result;
 use ebi_sources::source::Source;
+use futures::future::join_all;
 use reqwest;
 use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::Cursor;
-// use tokio::task::JoinHandle;
+use tokio::task::JoinHandle;
 
 pub async fn download_chapter(
     source: Box<dyn Source>,
@@ -31,34 +32,44 @@ pub async fn download_chapter(
 
     println!("Download Chapter: {:?}", chapter);
 
-    // let mut tasks: Vec<JoinHandle<Result<(), DownloadError>>> = vec![];
+    let mut tasks: Vec<JoinHandle<Result<(), DownloadError>>> = vec![];
 
     for page in pages {
         let url_parts = page.split("/").collect::<Vec<&str>>();
         let file_name = url_parts.last().unwrap();
         println!("{:?}", format!("{}::::{}", page, file_name));
-        // tasks.push(
-        // tokio::spawn(async move {
-        match reqwest::get(&page).await {
-            Ok(resp) => {
-                let mut out = File::create(format!(
-                    "{source_name}/{identifier}/{chapter}/{page}",
-                    source_name = source.identifier(),
-                    identifier = manga_identifier,
-                    chapter = chapter_number,
-                    page = file_name
-                ))
-                .expect("failed to create file");
-                let mut content = Cursor::new(resp.bytes().await?);
-                io::copy(&mut content, &mut out).expect("failed to copy content");
+        let destination = format!(
+            "{source_name}/{identifier}/{chapter}/{page}",
+            source_name = source.identifier(),
+            identifier = manga_identifier,
+            chapter = chapter_number,
+            page = file_name
+        );
+        let cloned_page = page.clone();
+        tasks.push(tokio::spawn(async move {
+            match download_page(page, destination).await {
+                Err(err) => {
+                    DownloadError::GenericError(format!(
+                        "ERROR downloading {}: {}",
+                        cloned_page, err
+                    ));
+                }
+                _ => {}
             }
-            Err(_) => {
-                DownloadError::GenericError(format!("ERROR downloading {}", page));
-            }
-        }
-        // })
-        // );
+            Ok(())
+        }));
     }
 
+    join_all(tasks).await;
+
+    Ok(())
+}
+
+async fn download_page(page: String, destination: String) -> Result<(), DownloadError> {
+    let resp = reqwest::get(page).await?;
+
+    let mut out = File::create(destination).expect("failed to create file");
+    let mut content = Cursor::new(resp.bytes().await?);
+    io::copy(&mut content, &mut out).expect("failed to copy content");
     Ok(())
 }
