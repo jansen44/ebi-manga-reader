25bc4b26013afd15cd74909147e374a86fae4cea
Vinicius Lopes <vlopes45@gmail.com>
2022-05-02 19:47:11 -0400

    :fire: Remove dead code
    Co-authored-by: Jansen Saunier <jsd.alcantara@gmail.com>

--- ebi_archive/examples/opex.rs
+++ ebi_archive/examples/opex.rs
@@ -8,28 +8,6 @@ async fn main() -> Result<()> {
 
     download_chapter(opex, "main", 7).await?;
 
-    // // let mangas = opex.mangas().await?;
-    // let manga = opex.manga("main").await?.unwrap();
-    // // let chapters = opex.chapters(&manga).await?;
-    // let chapter = opex.chapter(&manga, 7).await?.unwrap();
-    // let pages = opex.pages(&chapter).await?;
-    //
-    // println!("Source: {:?}", Opex::source());
-    //
-    // // for m in mangas {
-    // //     println!("{:?}", m);
-    // // }
-    // println!("Manga: {:?}", manga);
-    //
-    // for chapter in chapters {
-    //
-    // }
-    //
-    // println!("Download Chapter 7: {:?}", chapter);
-    //
-    // for page in pages {
-    //     println!("{:?}", page);
-    // }
 
     Ok(())
 }
