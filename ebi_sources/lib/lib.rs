11e4978a7bcea139c64cd7de2d0495c99ec60aa0
Jansen Junior <jsd.alcantara@gmail.com>
2022-05-07 23:59:58 -0400

    chore: add TODO comment

--- ebi_sources/lib/lib.rs
+++ ebi_sources/lib/lib.rs
@@ -14,6 +14,7 @@ pub use errors::Result;
 use source::Source;
 
 pub fn get_available_sources() -> HashMap<String, Box<dyn Source>> {
+    // TODO: Insert only sources selected by crate features
     let mut map = HashMap::<String, Box<dyn Source>>::new();
     map.insert("opex".to_owned(), Box::new(opex::OpexSource::default()));
     map.insert("yabu".to_owned(), Box::new(yabu::YabuSource::default()));
