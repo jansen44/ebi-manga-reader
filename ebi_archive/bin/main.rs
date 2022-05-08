46c7c66e77bc548e40aece1fb2a5cc1e5b71fc0a
Jansen Junior <jsd.alcantara@gmail.com>
2022-05-08 10:39:59 -0400

    feat: list sources

--- ebi_archive/bin/main.rs
+++ ebi_archive/bin/main.rs
@@ -1,6 +1,23 @@
+use ebi_sources::get_available_sources;
+use ebi_archive::errors::Result;
+
 mod args;
 
-fn main() {
-    let matches = args::get_args();
-    println!("{:?}", matches);
+fn handle_sources() {
+    println!("== Available Sources ==");
+    for (identifier, source) in get_available_sources().iter() {
+        println!("{identifier} -- {} -- {} -- {}/", source.title(), source.description(), source.base_url());
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let arg_matches = args::get_args();
+
+    match arg_matches.subcommand() {
+        Some(("sources", _)) => handle_sources(),
+        _ => ()
+    }
+
+    Ok(())
 }
